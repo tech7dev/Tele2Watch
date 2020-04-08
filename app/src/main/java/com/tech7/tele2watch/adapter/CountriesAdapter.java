947638f48f8395a9728735cd7c2a171fa4a3da98
tech7dev <tech7dev@gmail.com>
2020-04-08 23:30:23 +0200

    Commit 5

--- app/src/main/java/com/tech7/tele2watch/adapter/CountriesAdapter.java
+++ app/src/main/java/com/tech7/tele2watch/adapter/CountriesAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,23 +15,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.tech7.tele2watch.R;
 import com.tech7.tele2watch.object.Country;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.ViewHolder> {
-    LayoutInflater inflater;
-    List<Country> countries;
+public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.ViewHolder> implements Filterable {
+    private LayoutInflater inflater;
+    private List<Country> countries;
+    private List<Country> countriesListFull;
+
     private RecyclerViewClickListenerCountry listener;
 
-    public CountriesAdapter(Context ctx, List<Country> countries, RecyclerViewClickListenerCountry listener){
+    public CountriesAdapter(Context ctx, List<Country> countries, RecyclerViewClickListenerCountry listener) {
         this.inflater = LayoutInflater.from(ctx);
         this.countries = countries;
         this.listener = listener;
+        countriesListFull = new ArrayList<>(countries);
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.list_item_countries,parent,false);
+        View view = inflater.inflate(R.layout.list_item_countries, parent, false);
         return new ViewHolder(view, listener);
     }
 
@@ -46,12 +52,12 @@ public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.View
         return countries.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         TextView txtCountryCode;
         TextView txtCountry;
         LinearLayout parentLayout;
 
-        public ViewHolder(@NonNull View itemView, final RecyclerViewClickListenerCountry listener){
+        public ViewHolder(@NonNull View itemView, final RecyclerViewClickListenerCountry listener) {
             super(itemView);
             txtCountryCode = itemView.findViewById(R.id.txtCountryCode);
             txtCountry = itemView.findViewById(R.id.txtCountry);
@@ -60,7 +66,7 @@ public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.View
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(listener != null)
+                    if (listener != null)
                         listener.onCountryRowClicked(getAdapterPosition());
                 }
             });
@@ -68,7 +74,7 @@ public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.View
             parentLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(listener != null)
+                    if (listener != null)
                         listener.onCountryViewClicked(v, getAdapterPosition());
                 }
             });
@@ -83,6 +89,41 @@ public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.View
     public interface RecyclerViewClickListenerCountry {
 
         void onCountryRowClicked(int position);
+
         void onCountryViewClicked(View v, int position);
     }
+
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Country> filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(countriesListFull);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (Country country : countriesListFull) {
+                    if (country.getCountry().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(country);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            countries.clear();
+            countries.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
 }
